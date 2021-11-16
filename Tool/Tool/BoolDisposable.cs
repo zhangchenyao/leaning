2670efceec2aa087b449b14368b965d82ba2456a
“zcy” <chenyao.zhang@bjtct.com>
2021-11-16 16:58:53 +0800

    00

---
+++ Tool/Tool/BoolDisposable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool
+{
+    class BoolDisposable : IDisposable
+    {
+        private bool disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        ~BoolDisposable()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool dispoing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+            if (dispoing)
+            { 
+            
+            }
+        }
+    }
+}
---
+++ Tool/Tool/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Tool
+{
+    public class Class1
+    {
+    }
+}
